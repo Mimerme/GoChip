f7ebd2a9b135a6c1910e0e075da8444f8d800552
Andros Yang <androssdragonq.9@gmail.com>
2018-03-01 11:17:42 -0500

    Added stack and register UI

---
+++ debug/debug_ui.go
@@ -0,0 +1,51 @@
+package main
+
+import "github.com/marcusolsson/tui-go"
+
+func main() {
+
+	reg_label := tui.NewVBox(
+		tui.NewLabel("Registers"),
+	)
+	reg_label.SetSizePolicy(tui.Minimum, tui.Maximum)
+
+	stack_label := tui.NewVBox(
+		tui.NewLabel("Stack"),
+	)
+	stack_label.SetSizePolicy(tui.Minimum, tui.Maximum)
+
+	footer := tui.NewVBox(
+		tui.NewLabel("<Tab> to cycle through panels"),
+	)
+	footer.SetSizePolicy(tui.Minimum, tui.Maximum)
+
+	registers := tui.NewVBox(reg_label)
+	stack := tui.NewVBox(stack_label)
+	footer_box := tui.NewVBox(footer)
+
+	status_bar := tui.NewStatusBar("Registers & Stack")
+	status_bar.SetPermanentText("Chip 8 Debugging Tools")
+
+	main_panel := tui.NewVBox(
+		status_bar,
+		tui.NewHBox(registers, stack),
+		footer_box,
+	)
+
+	registers.SetBorder(true)
+	stack.SetBorder(true)
+
+	//Draw the stack
+	draw_stack(stack, nil)
+	draw_registers(registers, nil, nil, nil, nil)
+
+	ui, err := tui.New(main_panel)
+	if err != nil {
+		panic(err)
+	}
+	ui.SetKeybinding("Esc", func() { ui.Quit() })
+
+	if err := ui.Run(); err != nil {
+		panic(err)
+	}
+}
---
+++ debug/stack_pannel.go
@@ -0,0 +1,51 @@
+package main
+
+import "github.com/marcusolsson/tui-go"
+import (
+	"fmt"
+	"image"
+	"strconv"
+)
+
+func draw_stack(draw_pane *tui.Box, stack *[16]uint16) {
+	//4 bytes on each row
+	//2 bytes for address
+	//2 bytes for actually return addres after subroutine
+	//16 rows of data
+	grid := tui.NewGrid(2, 17)
+	grid.SetBorder(true)
+
+	address_label := tui.NewLabel("Stack Pointer")
+	address_label.SetSizePolicy(tui.Minimum, tui.Minimum)
+
+	value_label := tui.NewLabel("Value")
+	value_label.SetSizePolicy(tui.Minimum, tui.Minimum)
+
+	grid.SetCell(image.Point{X: 0, Y: 0}, address_label)
+	grid.SetCell(image.Point{X: 1, Y: 0}, value_label)
+
+	//Read values
+	for i := 0; i < 16; i++ {
+		grid.SetCell(image.Point{X: 0, Y: i + 1}, tui.NewLabel(strconv.Itoa(i)))
+		//TODO: Implement reading the stack
+	}
+
+	grid.SetSizePolicy(tui.Minimum, tui.Minimum)
+	draw_pane.Append(grid)
+}
+
+func draw_registers(draw_pane *tui.Box, GPRs *[16]byte, I *uint16, PC *uint16, SP *byte) {
+	grid := tui.NewGrid(4, 16)
+	grid.SetBorder(true)
+
+	for i := 0; i < 16; i++ {
+		grid.SetCell(image.Point{X: 0, Y: i}, tui.NewLabel(fmt.Sprintf("V[%X]", i)))
+		//TODO: Implement reading the stack
+	}
+	grid.SetCell(image.Point{X: 2, Y: 0}, tui.NewLabel("I"))
+	grid.SetCell(image.Point{X: 2, Y: 1}, tui.NewLabel("PC"))
+	grid.SetCell(image.Point{X: 2, Y: 2}, tui.NewLabel("SP"))
+
+	grid.SetSizePolicy(tui.Minimum, tui.Minimum)
+	draw_pane.Append(grid)
+}
